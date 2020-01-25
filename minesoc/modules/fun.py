030c5c48b2b727314fba67862814c3cd4e84be60
trumully <truman.mulholland@gmail.com>
2020-01-25 18:18:38 +1300

    update to discord.py 1.3

--- minesoc/modules/fun.py
+++ minesoc/modules/fun.py
@@ -5,6 +5,10 @@ import binascii
 from discord.ext import commands
 
 
+def binary_to_decimal(binary):
+    return int(binary, 2)
+
+
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -16,9 +20,12 @@ class Fun(commands.Cog):
             await ctx.send("Invalid command.")
 
     @binary.command()
-    async def a2b(self, ctx, *, string: str):
+    async def a2b(self, ctx, *, string):
         """Convert a string to binary"""
-        result = " ".join(format(ord(x), "b") for x in string)
+        if isinstance(string, int):
+            result = f"{int(string):08b}"
+        else:
+            result = " ".join(format(ord(x), "b") for x in string)
 
         embed = discord.Embed()
         if len(result) >= 300:
@@ -33,56 +40,58 @@ class Fun(commands.Cog):
 
         await ctx.send(embed=embed)
 
+    @binary.command()
+    async def b2a(self, ctx, *, bin_data):
+        bin_data = str(bin_data).replace(" ", "")
+
+        str_data = ""
+
+        for i in range(0, len(bin_data), 7):
+            temp_data = bin_data[i:i + 7]
+            decimal_data = binary_to_decimal(temp_data)
+            str_data = str_data + chr(decimal_data)
+
+        embed = discord.Embed()
+        if len(str_data) >= 300:
+            embed.colour = self.bot.colors.red
+            embed.title = "Output can't exceed 300 characters!"
+            await ctx.message.add_reaction("❗")
+        else:
+            embed.colour = self.bot.colors.neutral
+            embed.title = f"{bin_data} ->"
+            embed.description = f"**{str_data}**"
+            await ctx.message.add_reaction("👌")
+
+        await ctx.send(embed=embed)
+
+
     @commands.command()
     async def cat(self, ctx):
         """Returns a random image of a cat."""
         async with ctx.typing():
-            async with aiohttp.ClientSession() as session:
-                async with session.get("https://aws.random.cat/meow") as r:
-                    data = await r.json()
-
-            embed = discord.Embed()
-            embed.set_image(url=data["file"])
-            await ctx.send(embed=embed)
+            await ctx.send(embed=(await self.bot.api.animal.fetch_cat()).embed)
 
     @commands.command()
     async def dog(self, ctx, breed: str = None, sub_breed: str = None):
         """Returns a random image of a dog."""
         async with ctx.typing():
-            if not breed:
-                url = "https://dog.ceo/api/breeds/image/random"
-            elif not sub_breed:
-                url = f"https://dog.ceo/api/breed/{breed}/images/random"
-            else:
-                url = f"https://dog.ceo/api/breed/{breed}/{sub_breed}/images/random"
-
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as r:
-                    data = await r.json()
-
-            embed = discord.Embed()
-            if data["status"] == "success":
-                embed.set_image(url=data["message"])
-            else:
-                embed.colour = self.bot.colors.red
-                embed.title = data["message"]
-            await ctx.send(embed=embed)
+            await ctx.send(embed=(await self.bot.api.animal.fetch_dog(breed, sub_breed)).embed)
 
     @commands.group(aliases=["da", "devart"])
     async def deviantart(self, ctx):
-        """Offers a selection of options to browse DeviantArt."""
+        """Offers options to browse DeviantArt."""
         if ctx.invoked_subcommand is None:
             await ctx.send("Invalid command.")
 
     @deviantart.command()
     async def tag(self, ctx, tag):
-        """Get a random Deviant by tag."""
+        """Get a Deviant by tag."""
         async with ctx.typing():
             await ctx.send(embed=(await self.bot.api.deviantart.browse_tags(tag)).embed)
 
     @deviantart.command()
     async def popular(self, ctx, query: str = None, category: str = None):
-        """Get a random Deviant by popularity."""
+        """Get a Deviant by popularity."""
         async with ctx.typing():
             await ctx.send(embed=(await self.bot.api.deviantart.browse_popular(query, category)).embed)
 
--- minesoc/utils/api.py
+++ minesoc/utils/api.py
@@ -3,8 +3,9 @@ import json
 import aiohttp
 import asyncio
 import deviantart
+import re
 
-from discord.ext import tasks
+from discord.ext import commands
 from libneko.aggregates import Proxy
 from dotenv import dotenv_values
 from random import choice
@@ -18,6 +19,7 @@ class API:
     def __init__(self, bot):
         self.session = aiohttp.ClientSession()
         self.deviantart = DeviantArt(self.session)
+        self.animal = Animal(self.session)
         self.bot = bot
 
 
@@ -29,25 +31,32 @@ class DeviantArt:
                 self._info = choice(response["results"])
                 self.mature = self._info["is_mature"]
                 self.deviation_id = self._info["deviationid"]
+
                 self.name = self._info["title"]
-                self.author = self._info["author"]["username"]
-                self.image = self._info["content"]["src"]
+                self.author = self._info["author"]
+                self.username = self.author["username"]
+                self.user_icon = self.author["usericon"]
+
+                self.content = self._info["content"]
+                self.image = self.content["src"]
+                self.resolution = f"{self.content['width']} x {self.content['height']}"
+
                 self.url = self._info["url"]
                 self.stats = self._info["stats"]
                 self.category = self._info["category"]
-                self.resolution = f"{self._info['content']['width']} x {self._info['content']['height']}"
 
                 self.embed = self.__generate_embed()
+
             else:
                 self.embed = error
 
         def __generate_embed(self):
-            embed = discord.Embed(color=discord.Color.blue(), title=self.name, description=f"by {self.author}")
+            embed = discord.Embed(color=discord.Color.blue(), title=self.name, url=self.url)
+            embed.set_author(name=self.username, icon_url=self.user_icon)
             embed.set_image(url=self.image)
-            embed.set_footer(text=self.url)
-            embed.add_field(name="Category", value=self.category)
-            embed.add_field(name="Stats", value=f"Comments: {self.stats['comments']}\n"
-                                                f"Favorites: {self.stats['favourites']}")
+            embed.add_field(name="Category:", value=self.category)
+            embed.add_field(name="Stats:", value=f"💬 {self.stats['comments']} | ⭐ {self.stats['favourites']}")
+            embed.add_field(name="Resolution:", value=self.resolution)
 
             return embed
 
@@ -59,35 +68,27 @@ class DeviantArt:
 
     async def browse_tags(self, tags: str):
         tags = tags.lower()
-        async with self.session.get(
-                self.base_url + f"browse/tags?tag={tags}&limit=50&access_token={self.access_token}") as r:
+        url = self.base_url + f"browse/tags?tag={tags}&limit=50&access_token={self.access_token}"
+        async with self.session.get(url) as r:
             if r.status == 200:
                 return self.DeviantArtResponse(response=await r.json())
             else:
                 response = await r.json()
                 error = f"{response['error']}: {response['error_description']}"
+                if response["error"] == "invalid_token":
+                    self.da = deviantart.Api(env_values.DEVIANTART_CLIENT_ID, env_values.DEVIANTART_CLIENT_SECRET)
+                    self.access_token = self.da.access_token
                 embed = discord.Embed(color=discord.Color.red(), title="An error has occurred!",
-                                      description=f"`{error}`")
-                if r.status == 400:
-                    print("DeviantArt Error 400: Request failed due to client error")
-                elif r.status == 429:
-                    print("DeviantArt Error 429: Rate limit reached or service overloaded")
-                elif r.status == 500:
-                    print("DeviantArt Error 500: Our servers encountered an internal error, try again")
-                elif r.status == 503:
-                    print("DeviantArt Error 503: Our servers are currently unavailable, try again later. "
-                          "This is normally due to planned or emergency maintenance.")
-                else:
-                    print("DeviantArt Unexpected Error: An unexpected error has occurred.")
+                                      description=f"Error {r.status}\n`{error}`")
 
                 return self.DeviantArtResponse(response=None, error=embed)
 
-    async def browse_popular(self, query, category):
+    async def browse_popular(self, query: str = None, category: str = None):
         url = self.base_url + "browse/popular?"
-        if category is not None:
+        if category:
             category = category.lower()
             url += f"category_path={category}&"
-        if query is not None:
+        if query:
             query = query.lower()
             url += f"q={query}&"
         url += f"timerange=1week&limit=50&access_token={self.access_token}"
@@ -97,18 +98,75 @@ class DeviantArt:
             else:
                 response = await r.json()
                 error = f"{response['error']}: {response['error_description']}"
+                if response["error"] == "invalid_token":
+                    self.da = deviantart.Api(env_values.DEVIANTART_CLIENT_ID, env_values.DEVIANTART_CLIENT_SECRET)
+                    self.access_token = self.da.access_token
                 embed = discord.Embed(color=discord.Color.red(), title="An error has occurred!",
-                                      description=f"`{error}`")
-                if r.status == 400:
-                    print("DeviantArt Error 400: Request failed due to client error")
-                elif r.status == 429:
-                    print("DeviantArt Error 429: Rate limit reached or service overloaded")
-                elif r.status == 500:
-                    print("DeviantArt Error 500: Our servers encountered an internal error, try again")
-                elif r.status == 503:
-                    print("DeviantArt Error 503: Our servers are currently unavailable, try again later. "
-                          "This is normally due to planned or emergency maintenance.")
-                else:
-                    print("DeviantArt Unexpected Error: An unexpected error has occurred.")
+                                      description=f"Error {r.status}\n`{error}`")
 
                 return self.DeviantArtResponse(response=None, error=embed)
+
+
+class Animal:
+
+    class DogResponse:
+        def __init__(self, response, error: discord.Embed = None):
+            if error is None:
+                self._info = response
+                self.status = self._info["status"]
+                self.url = self._info["message"]
+                breed_search = re.search("breeds/(.*)/", self.url)
+                self.breed = breed_search.group(1).replace("-", " ").title()
+
+                self.embed = self.__generate_embed()
+
+            else:
+                self.embed = error
+
+        def __generate_embed(self):
+            embed = discord.Embed(color=discord.Color.blue())
+            embed.title = self.breed
+            embed.set_image(url=self.url)
+            return embed
+
+    class CatResponse:
+        def __init__(self, response, error: discord.Embed = None):
+            if error is None:
+                self.image = response["file"]
+                self.embed = self.__generate_embed()
+            else:
+                self.embed = error
+
+        def __generate_embed(self):
+            embed = discord.Embed(color=discord.Color.blue())
+            embed.set_image(url=self.image)
+
+            return embed
+
+    def __init__(self, session: aiohttp.ClientSession()):
+        self.session = session
+        self.dog_url = "https://dog.ceo/api/breed"
+        self.cat_url = "https://aws.random.cat/meow"
+
+    async def fetch_dog(self, breed: str = None, sub_breed: str = None):
+        if breed and sub_breed:
+            url = self.dog_url + f"/{breed.lower()}/{sub_breed.lower()}/images/random"
+        elif breed:
+            url = self.dog_url + f"/{breed.lower()}/images/random"
+        else:
+            url = self.dog_url + "s/image/random"
+        async with self.session.get(url) as r:
+            response = await r.json()
+            if response["status"] == "success":
+                return self.DogResponse(response=response)
+            else:
+                embed = discord.Embed(color=discord.Color.red(), title="Error!", description=response["message"])
+                return self.DogResponse(response=None, error=embed)
+
+    async def fetch_cat(self):
+        async with self.session.get(self.cat_url) as r:
+            if r.status == 200:
+                return self.CatResponse(response=await r.json())
+            else:
+                embed = discord.Embed(color=discord.Color.red(), title="An error has occurred! Try again later.")
+                return self.CatResponse(response=None, error=embed)
